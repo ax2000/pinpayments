74bfad850a7fb7c4e71e9ee02339d4cb8bbbd6b8
Alberto Martinez <a.martinez@marby.com.au>
2023-04-07 18:54:22 +1000

    base plugin structure after craft generator creation

---
+++ README.md
@@ -0,0 +1,8 @@
+# Pin Payments
+
+Pin Payments gateway integration
+
+## Requirements
+
+This plugin requires Craft CMS 4.4.5 or later, and PHP 8.0.2 or later.
+
---
+++ src/models/Settings.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace pixelpie\craftpinpayments\models;
+
+use Craft;
+use craft\base\Model;
+
+/**
+ * Pin Payments settings
+ */
+class Settings extends Model
+{
+}
